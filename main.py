1762129489dd995a7f1f16b5bb1a61275cadcc9c
esbykov <esbykov@gmail.com>
2025-07-31 12:52:10 +0300

    Добавлена обработка исключений

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ from opencage.geocoder import OpenCageGeocode
 
 
 def get_coordinates(city, key):
-    geocoder = OpenCageGeocode(key)
-    query = city
-    results = geocoder.geocode(query)
-    if results:
-        return results[0]['geometry']['lat'], results[0]['geometry']['lng']
-    else:
-        return "Город не найден"
+    try:
+        geocoder = OpenCageGeocode(key)
+        query = city
+        results = geocoder.geocode(query)
+        if results:
+                return results[0]['geometry']['lat'], results[0]['geometry']['lng']
+        else:
+            return "Город не найден"
+    except Exception as e:
+        return f"Общая ошибка: {e}"
 
 key = '63e5eb15415b4679a88d546ae3e03d01'
-city = "Moscow"
+city = "London"
 coordinates = get_coordinates(city, key)
 print(f"Координаты города {city}: {coordinates}")
 
-    
+
 
 
